f80f8fa70ff5e4e048c8038a4a696ebc790ae920
Tim Keefer <tkeefer@gmail.com>
2012-05-19 16:55:19 -0700

    new conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -171,10 +171,10 @@ htmlhelp_basename = 'ebayapidoc'
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
-latex_documents = [
-  ('index', 'ebayapi.tex', u'eBay API Simple Documentation',
-   u'Tim Keefer', 'manual'),
-]
+#latex_documents = [
+#  ('index', 'ebayapi.tex', u'eBay API Simple Documentation',
+#   u'Tim Keefer', 'manual'),
+#]
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
